a52365de054905f89a22a7ffbc81d87aaf5a400d
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-05-11 16:57:30 +0800

    hh

---
+++ src/pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="content">
+   
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello"
+    };
+  },
+  onLoad() {
+    // wx.cloud
+    //   .callFunction({
+    //     name: "login"
+    //   })
+    //   .then(res => {
+    //     console.log("用户信息", res);
+    //   })
+    //   .catch(console.log);
+    this.getDate();
+  },
+  methods: {
+    getDate() {
+      // 查询数据库
+      const db = wx.cloud.database();
+      // 查询当前用户所有的 counters
+      db.collection("money")
+        .where({})
+        .get({
+          success: res => {
+            console.log(res)
+          },
+          fail: () => {
+            Taro.showToast({
+              icon: "none",
+              title: "查询记录失败"
+            });
+          }
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.aa {
+  color:red;
+}
+</style>
---
+++ src/pages/money/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <view class="money">
+  
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello"
+    };
+  },
+  onLoad() {
+    this.getDate();
+  },
+  methods: {
+    getDate() {
+      // 查询数据库
+      const db = wx.cloud.database();
+      // 查询当前用户所有的 counters
+      db.collection("money")
+        .where({})
+        .get({
+          success: res => {
+            console.log(res);
+          },
+          fail: () => {
+            Taro.showToast({
+              icon: "none",
+              title: "查询记录失败"
+            });
+          }
+        });
+    }
+  }
+};
+</script>
+
+<style>
+.content {
+  text-align: center;
+  height: 400upx;
+}
+
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
+
+.title {
+  font-size: 36upx;
+  color: #8f8f94;
+}
+</style>
